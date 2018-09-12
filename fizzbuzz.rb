69bd3e9aff73b3ea0e4bfce91278dabe7d3810fc
Amir Safoev <amirsafoev0@gmail.com>
2018-09-12 06:02:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0
-    "Fizz"
+   puts "Fizz"
   elsif int % 5 == 0
-    "Buzz"
+    puts "Buzz"
   elsif (int % 5 == 0) && (int % 3 == 0)
-     'FizzBuzz'
+     puts 'FizzBuzz'
   else 
     nil
     end
